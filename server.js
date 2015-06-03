2844100b94c57d5eeb7911c5ea343ca7b4bfc988
naveen.kada <naveen.kada@gmail.com>
2015-06-03 13:33:46 +0100

    3rd commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!!</h1>');
+    res.end('<h1>Hello Code and Azure Web Apps!!</h1><h2>What next</h2');
 }).listen(process.env.PORT);
